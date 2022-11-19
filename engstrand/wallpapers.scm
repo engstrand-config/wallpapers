d876bfd1f49fe58cca185d20b5b57ae5c93d208e
Fredrik Engstrand <fredrik@engstrand.nu>
2022-11-19 10:28:51 +0100

    feat: add guile util for getting absolute wallpaper path

---
+++ engstrand/wallpapers.scm
@@ -0,0 +1,12 @@
+(define-module (engstrand wallpapers)
+  #:use-module (guix gexp)
+  #:use-module (srfi srfi-1))
+
+;; Find the absolute path to this file.
+(define %wallpapers-root-dir
+  (find (lambda (path)
+          (file-exists? (string-append path "/engstrand/" current-filename)))
+        %load-path))
+
+(define-public (get-wallpaper-path wallpaper)
+  (string-append %wallpapers-root-dir "/engstrand/wallpapers/" wallpaper))
